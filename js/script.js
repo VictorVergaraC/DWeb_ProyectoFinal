885b14fffa88d45a1481a23d8971ecb2b7e4b6a8
vvergara <victorvergara.c99@gmail.com>
2023-08-21 15:11:41 -0400

    Se agrega nueva vista de preguntas frecuentes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,28 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const acordionItems = document.querySelectorAll(".acordion-item")
+
+    if (acordionItems) {
+        acordionItems.forEach((item) => {
+            const header = item.querySelector(".acordion-header")
+            const content = item.querySelector(".acordion-content")
+    
+            header.addEventListener("click", () => {
+                item.classList.toggle("active");
+    
+                if (item.classList.contains("active")) {
+                    content.style.maxHeight = content.scrollHeight + "px"
+                } else {
+                    content.style.maxHeight = "0"
+                }
+            });
+    
+            if (window.innerWidth <= 768) {
+                content.style.maxHeight = "0"
+            }
+        })
+    }
+})
+
 AOS.init();
 
 let toggleButtons = document.querySelectorAll('.toggle-button');
