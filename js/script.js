be75bd5773fca34b52b5485e0c6f172e0089862e
vvergara <victorvergara.c99@gmail.com>
2023-08-21 15:19:47 -0400

    Se cambia el color y se le da estilos a los acordion-item

--- js/script.js
+++ js/script.js
@@ -5,17 +5,17 @@ document.addEventListener("DOMContentLoaded", function () {
         acordionItems.forEach((item) => {
             const header = item.querySelector(".acordion-header")
             const content = item.querySelector(".acordion-content")
-    
+
             header.addEventListener("click", () => {
                 item.classList.toggle("active");
-    
+
                 if (item.classList.contains("active")) {
                     content.style.maxHeight = content.scrollHeight + "px"
                 } else {
                     content.style.maxHeight = "0"
                 }
             });
-    
+
             if (window.innerWidth <= 768) {
                 content.style.maxHeight = "0"
             }
